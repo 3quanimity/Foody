15ab3c360ea51ba23eb3d5ecbca05c2514455f32
Haythem Ben Drissia <hbendrissia@vermeg.com>
2021-07-09 10:13:25 +0100

    output list of recipes using ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipeModule } from '../recipe.module';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: RecipeModule[]= [
+    new RecipeModule("Test Recipe", "This is just a test", "https://cdn.pixabay.com/photo/2015/08/25/03/50/background-906135_960_720.jpg"),
+    new RecipeModule("Test Recipe", "This is just a test", "https://cdn.pixabay.com/photo/2015/08/25/03/50/background-906135_960_720.jpg"),
+    new RecipeModule("Test Recipe", "This is just a test", "https://cdn.pixabay.com/photo/2015/08/25/03/50/background-906135_960_720.jpg")
+  ];
 
   constructor() { }
 
