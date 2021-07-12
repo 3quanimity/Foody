01938fe12ab0bf0a836a9e1f18142dbdb49c42a5
Haythem Ben Drissia <hbendrissia@vermeg.com>
2021-07-12 17:36:46 +0100

    Add navigation w/ event binding & ngIf (exercice)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'foody';
+  destination: string = "";
+
+  updateDestination(newDestination){
+    this.destination = newDestination
+  }
 }
